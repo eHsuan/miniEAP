6453fda53ca506d53073ac6c32f803f9d5011735
eHsuan <ac131151@gmail.com>
2026-05-12 13:58:06 +0800

    UPDATE

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,60 +34,54 @@ namespace miniEAP
             this.cmbMultiplier.Items.AddRange(new object[] {
             "1",
             "2"});
-            this.cmbMultiplier.Location = new System.Drawing.Point(400, 30);
-            this.cmbMultiplier.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.cmbMultiplier.Location = new System.Drawing.Point(150, 12);
             this.cmbMultiplier.Name = "cmbMultiplier";
-            this.cmbMultiplier.Size = new System.Drawing.Size(316, 38);
+            this.cmbMultiplier.Size = new System.Drawing.Size(121, 20);
             this.cmbMultiplier.TabIndex = 0;
             this.cmbMultiplier.SelectedIndexChanged += new System.EventHandler(this.cmbMultiplier_SelectedIndexChanged);
             // 
             // lblMultiplier
             // 
             this.lblMultiplier.AutoSize = true;
-            this.lblMultiplier.Location = new System.Drawing.Point(32, 38);
-            this.lblMultiplier.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblMultiplier.Location = new System.Drawing.Point(12, 15);
             this.lblMultiplier.Name = "lblMultiplier";
-            this.lblMultiplier.Size = new System.Drawing.Size(253, 30);
+            this.lblMultiplier.Size = new System.Drawing.Size(101, 12);
             this.lblMultiplier.TabIndex = 1;
             this.lblMultiplier.Text = "曝光機一次跑幾片";
             // 
             // txtSend
             // 
-            this.txtSend.Location = new System.Drawing.Point(32, 150);
-            this.txtSend.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.txtSend.Location = new System.Drawing.Point(12, 60);
             this.txtSend.Multiline = true;
             this.txtSend.Name = "txtSend";
             this.txtSend.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtSend.Size = new System.Drawing.Size(2020, 444);
+            this.txtSend.Size = new System.Drawing.Size(690, 178);
             this.txtSend.TabIndex = 2;
             // 
             // txtReceive
             // 
-            this.txtReceive.Location = new System.Drawing.Point(32, 675);
-            this.txtReceive.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.txtReceive.Location = new System.Drawing.Point(12, 270);
             this.txtReceive.Multiline = true;
             this.txtReceive.Name = "txtReceive";
             this.txtReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtReceive.Size = new System.Drawing.Size(2020, 444);
+            this.txtReceive.Size = new System.Drawing.Size(690, 181);
             this.txtReceive.TabIndex = 3;
             // 
             // lblSend
             // 
             this.lblSend.AutoSize = true;
-            this.lblSend.Location = new System.Drawing.Point(32, 112);
-            this.lblSend.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblSend.Location = new System.Drawing.Point(12, 45);
             this.lblSend.Name = "lblSend";
-            this.lblSend.Size = new System.Drawing.Size(178, 30);
+            this.lblSend.Size = new System.Drawing.Size(70, 12);
             this.lblSend.TabIndex = 4;
             this.lblSend.Text = "Send Message";
             // 
             // lblReceive
             // 
             this.lblReceive.AutoSize = true;
-            this.lblReceive.Location = new System.Drawing.Point(32, 638);
-            this.lblReceive.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblReceive.Location = new System.Drawing.Point(12, 255);
             this.lblReceive.Name = "lblReceive";
-            this.lblReceive.Size = new System.Drawing.Size(214, 30);
+            this.lblReceive.Size = new System.Drawing.Size(84, 12);
             this.lblReceive.TabIndex = 5;
             this.lblReceive.Text = "Receive Message";
             // 
@@ -95,10 +89,9 @@ namespace miniEAP
             // 
             this.pnlHeartbeat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pnlHeartbeat.BackColor = System.Drawing.Color.Gray;
-            this.pnlHeartbeat.Location = new System.Drawing.Point(2005, 30);
-            this.pnlHeartbeat.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.pnlHeartbeat.Location = new System.Drawing.Point(678, 12);
             this.pnlHeartbeat.Name = "pnlHeartbeat";
-            this.pnlHeartbeat.Size = new System.Drawing.Size(53, 50);
+            this.pnlHeartbeat.Size = new System.Drawing.Size(20, 20);
             this.pnlHeartbeat.TabIndex = 6;
             // 
             // timerHeartbeat
@@ -113,9 +106,9 @@ namespace miniEAP
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 30F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2091, 1152);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(710, 462);
             this.Controls.Add(this.pnlHeartbeat);
             this.Controls.Add(this.lblReceive);
             this.Controls.Add(this.lblSend);
@@ -123,7 +116,6 @@ namespace miniEAP
             this.Controls.Add(this.txtSend);
             this.Controls.Add(this.lblMultiplier);
             this.Controls.Add(this.cmbMultiplier);
-            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.Name = "Form1";
             this.Text = "miniEAP";
             this.Load += new System.EventHandler(this.Form1_Load);
